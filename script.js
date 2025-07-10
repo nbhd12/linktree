680dace779c0f766a286a0e9f9ba1057eeb73127
nbhd12 <nupur124@gmail.com>
2025-07-10 18:29:36 +0200

    added links to div

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const linkone = document.getElementById("linkone");
+
+  linkone.addEventListener("click", function () {
+    window.open("https://github.com/nbhd12/", "_blank");
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const linktwo = document.getElementById("linktwo");
+
+  linktwo.addEventListener("click", function () {
+    window.open("https://www.linkedin.com/in/nupur-bhide/", "_blank");
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const linkthree = document.getElementById("linkthree");
+
+  linkthree.addEventListener("click", function () {
+    window.open("https://nupurb.framer.website/", "_blank");
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const linkfour = document.getElementById("linkfour");
+
+  linkfour.addEventListener("click", function () {
+    window.open("https://www.behance.net/nupur124", "_blank");
+  });
+});
